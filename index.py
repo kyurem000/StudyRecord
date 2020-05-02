9bbc8010cd80784ee7cac32921603169b54c5dae
kyurem000 <hanapsk61@knou.ac.kr>
2020-05-03 08:11:03 +0900

    index.py
    2020-05-03
    리스트의 리스트

---
+++ index.py
@@ -0,0 +1,18 @@
+# 한 줄 주석 
+
+""" 
+여러 줄 주석 
+"""
+
+# 리스트의 리스트  # 
+# a = ("man", "girl", "dog", "cat")
+# b = ("father", "mother", "bug", "crap")
+
+# dataA = (a, b)
+# print(dataA[0][1]) # girl이 출력 
+# print(dataA[0]) # ("man", "girl", "dog", "cat") 출력 
+
+len([1,2,3,4,5])
+
+
+
