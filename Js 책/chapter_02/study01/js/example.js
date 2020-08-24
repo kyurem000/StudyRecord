9c34c49280d5259f5dc6cd2ad97ab9c619869b77
Divide <hanapsk61@knou.ac.kr>
2020-08-25 01:52:12 +0900

    update
    2020-08-25

---
+++ Js 책/chapter_02/study01/js/example.js
@@ -0,0 +1,8 @@
+document.write("환영합니다.","<br>");
+
+var s; // undefined
+
+var abc = "abc의_타입은_string이다"
+var dex = 1920
+document.write(typeof abc,"<br>"); // 자료형을 알고 싶을 떄 
+document.write(typeof dex); 
